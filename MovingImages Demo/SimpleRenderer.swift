c95e9b2a3266a4baa903f2506aa0a9d615252602
Kevin Meaney <ktam@yvs.eu.com>
2015-05-04 16:04:00 +0100

    Added ability to add and remove spinners and have them take effect.

--- MovingImages Demo/SimpleRenderer.swift
+++ MovingImages Demo/SimpleRenderer.swift
@@ -7,7 +7,7 @@ import MovingImages
 
 
 class SimpleRendererWindowController:NSWindowController, NSTextViewDelegate,
-                                     SpinnerDelegate {
+                                     NSWindowDelegate, SpinnerDelegate {
     @IBOutlet var drawElementJSON: NSTextView!
     
     @IBOutlet weak var simpleRenderView: SimpleRendererView!
@@ -17,7 +17,27 @@ class SimpleRendererWindowController:NSWindowController, NSTextViewDelegate,
     @IBOutlet weak var spinnerThree: SpinnerController!
     @IBOutlet weak var spinnerFour: SpinnerController!
     @IBOutlet weak var exampleList: NSPopUpButton!
+    @IBOutlet weak var addSpinner: NSButton!
+    @IBOutlet weak var removeSpinner: NSButton!
 
+    @IBAction func addSpinner(sender: AnyObject) {
+        for spinner in spinners {
+            if spinner.view.hidden {
+                spinner.view.hidden = false
+                break
+            }
+        }
+    }
+    
+    @IBAction func removeSpinner(sender: AnyObject) {
+        for spinner in reverse(spinners) {
+            if !spinner.view.hidden {
+                spinner.view.hidden = true
+                break
+            }
+        }
+    }
+    
     @IBAction func exampleSelected(sender: AnyObject) {
         let popup = sender as! NSPopUpButton
         let selectedTitle = popup.titleOfSelectedItem!
@@ -62,9 +82,10 @@ class SimpleRendererWindowController:NSWindowController, NSTextViewDelegate,
         spinners.append(spinnerThree)
         spinners.append(spinnerFour)
 
+        for spinner in spinners {
+            spinner.delegate = self
+        }
         drawElementJSON.delegate = self
-        spinnerOne.delegate = self
-        spinnerTwo.delegate = self
         drawElementJSON.automaticQuoteSubstitutionEnabled = false
         drawElementJSON.font = NSFont(name: "Menlo-Regular", size: 11)
         drawElementJSON.textColor = NSColor(deviceWhite: 0.95, alpha: 1.0)
@@ -73,13 +94,17 @@ class SimpleRendererWindowController:NSWindowController, NSTextViewDelegate,
             NSBackgroundColorAttributeName : NSColor.lightGrayColor(),
             NSForegroundColorAttributeName : NSColor.blackColor()
         ]
-        simpleRenderView.variables = self.variables
+        // simpleRenderView.variables = self.variables
         if let theImage = createCGImage("Sculpture", fileExtension: "jpg") {
             simpleRenderView.assignImage(theImage, identifier: "Sculpture")
         }
         
         exampleList.addItemsWithTitles(listOfExamples(prefix: "simple_renderer_"))
         self.exampleSelected(exampleList)
+    }
+    
+    func windowDidResize(notification: NSNotification) {
+        simpleRenderView.variables = self.variables
     }
     
     func textDidChange(notification: NSNotification) {
