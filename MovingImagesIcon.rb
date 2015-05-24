4b12af302473cddb0fcb191505dc3c1d55842222
Kevin Meaney <ktam@yvs.eu.com>
2015-05-24 20:38:41 +0100

    Added the Zukini application Icon to the project with the ruby file that generates the json.

--- MovingImagesIcon.rb
+++ MovingImagesIcon.rb
@@ -3,29 +3,24 @@ require 'moving_images'
 include MovingImages
 include MICGDrawing
 
-# Variables for teardrop.
-#  * logowidth: the width of the logo in pixels
-#  * fraction: size of teardrop main stroke as fraction of logowidth. 0.2 - 0.6
-#  * 0.479: Radius of large arc.
-#  * 0.04: Radius of small arc.
 def make_teardroppath()
   thePath = MIPath.new
   centerPoint = MIShapes.make_point(0, 0)
   thePath.add_arc(centerPoint: centerPoint,
-         radius: "0.479 * $iconwidth * $fraction",
+         radius: "0.479 * $iconwidth / (2.966 * $scalefactor)",
      startAngle: "pi_2() - asin(0.479 - 0.04)",
        endAngle: "asin(0.479 - 0.04) - pi_2()")
 
-  centerPoint2 = MIShapes.make_point("$iconwidth * $fraction", 0)
+  centerPoint2 = MIShapes.make_point("$iconwidth / (2.966 * $scalefactor)", 0)
   thePath.add_arc(centerPoint: centerPoint2,
-         radius: "0.04 * $iconwidth * $fraction",
+         radius: "0.04 * $iconwidth / (2.966 * $scalefactor)",
      startAngle: "asin(0.479 - 0.04) - pi_2()",
        endAngle: "pi_2() - asin(0.479 - 0.04)")
   thePath.add_closesubpath()
 
   startPoint = MIShapes.make_point(
-        "$iconwidth * $fraction * (1.0 + 0.04 * (0.479 - 0.04))",
-        "$iconwidth * $fraction * 0.04 * cos(asin(0.479 - 0.04))")
+        "$iconwidth / (2.966 * $scalefactor) * (1.0 + 0.04 * (0.479 - 0.04))",
+        "$iconwidth / (2.966 * $scalefactor) * 0.04 * cos(asin(0.479 - 0.04))")
   return startPoint, thePath
 end
 
@@ -40,9 +35,9 @@ end
 
 def make_drawlogo()
   drawLogo = MIDrawElement.new(:arrayofelements)
-  drawLogo.fillcolor = MIColor.make_rgbacolor(0.8, 0.42, 0.14)
+  drawLogo.fillcolor = MIColor.make_rgbacolor(0.82, 0.53, 0.1)
 
-  offset = MIShapes.make_point("-$iconwidth * $fraction * 1.2", 0)
+  offset = MIShapes.make_point("-$iconwidth * ($scalefactor + 2.0) * 0.1667", 0)
   6.times do |i|
     transformations = MITransformations.make_contexttransformation()
     angle = i.to_f * 60.0 / 180.0 * Math::PI
@@ -52,11 +47,12 @@ def make_drawlogo()
     drawLogo.add_drawelement_toarrayofelements(drawElement)
   end
 
-  transformations3 = MITransformations.make_contexttransformation()
-  offset3 = MIShapes.make_point("$width * 0.5",
-                                "$height * 0.5")
-  MITransformations.add_translatetransform(transformations3, offset3)
-  drawLogo.contexttransformations = transformations3
+  transformationIcon = MITransformations.make_contexttransformation()
+  centrePoint = MIShapes.make_point("$width * 0.5",
+                                    "$height * 0.5")
+  MITransformations.add_translatetransform(transformationIcon, centrePoint)
+  MITransformations.add_rotatetransform(transformationIcon, "$angle")
+  drawLogo.contexttransformations = transformationIcon
   drawLogo
 end
 
