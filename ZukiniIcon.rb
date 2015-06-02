d85034da3e3c985bfd6b54b4039c13b7b0d591be
Kevin Meaney <ktam@yvs.eu.com>
2015-06-03 00:43:04 +0100

    Updating scripts for creating application icons and added the application icons to MovingImages Demo, and Zukini Demo.

--- ZukiniIcon.rb
+++ ZukiniIcon.rb
@@ -2,6 +2,7 @@ require 'moving_images'
 
 include MovingImages
 include MICGDrawing
+include CommandModule
 
 def make_teardroppath(iconwidth)
   thePath = MIPath.new
@@ -31,7 +32,8 @@ def make_drawteardrop(transformations, iconwidth)
   pathDrawElement
 end
 
-def make_drawlogo(iconwidth)
+def make_drawlogo(bitmapsize)
+  iconwidth = 0.74074074 * bitmapsize
   drawLogo = MIDrawElement.new(:arrayofelements)
   drawLogo.fillcolor = MIColor.make_rgbacolor(0.82, 0.53, 0.1)
 
@@ -47,13 +49,64 @@ def make_drawlogo(iconwidth)
 
   transformationIcon = MITransformations.make_contexttransformation()
 #  centrePoint = MIShapes.make_point("$width * 0.5", "$height * 0.5")
-  modIconWidth = iconwidth * 1.35 # 1/1.35 = 0.74074074
-  centrePoint = MIShapes.make_point(modIconWidth * 0.5, modIconWidth * 0.5)
+#  modIconWidth = iconwidth * 1.35 # 1/1.35 = 0.74074074
+  centrePoint = MIShapes.make_point(bitmapsize * 0.5, bitmapsize * 0.5)
   MITransformations.add_translatetransform(transformationIcon, centrePoint)
   MITransformations.add_rotatetransform(transformationIcon, 0.526)
   drawLogo.contexttransformations = transformationIcon
   drawLogo
 end
 
-drawTearDrop = make_drawlogo(370.0)
-puts JSON.pretty_generate(drawTearDrop.elementhash)
+def make_fillbackground_drawelement(bitmap, bitmapSize, color)
+  rect = MIShapes.make_rectangle(xloc: 0, yloc: 0, size: bitmapSize)
+  drawElement = MIDrawElement.new(:fillrectangle)
+  drawElement.fillcolor = color
+  drawElement.rectangle = rect
+  drawElement.blendmode = :kCGBlendModeCopy
+  drawElement
+end
+
+def make_transparentpngicon()
+  # Constants
+  logoSize = 128
+  bitmapSize = MIShapes.make_size(logoSize, logoSize)
+  
+  # Command list setup
+  logoCommands = SmigCommands.new
+  logoCommands.saveresultstype = :lastcommandresult
+  
+  # Make the create bitmap context command
+  bitmap = logoCommands.make_createbitmapcontext(size: bitmapSize,
+                      preset: :PlatformDefaultBitmapContext)
+
+  # Make the background transparent.
+  color = MIColor.make_rgbacolor(0,0,0, a: 0)
+  drawElement = make_fillbackground_drawelement(bitmap, bitmapSize, color)
+  setBackgroundTransparent = CommandModule.make_drawelement(bitmap,
+                                              drawinstructions: drawElement)
+  logoCommands.add_command(setBackgroundTransparent)
+
+  # Make the create export command.
+  exporter = logoCommands.make_createexporter(
+                      "~/Desktop/ZukiniIconTransparent#{logoSize}.png",
+         export_type: :"public.png")
+
+  # Create the drawing commands
+  drawLogo = make_drawlogo(logoSize)
+  drawCommand = CommandModule.make_drawelement(bitmap,
+                             drawinstructions: drawLogo)
+  logoCommands.add_command(drawCommand)
+  
+  # Add the image to the exporter object
+  add_image = CommandModule.make_addimage(exporter, bitmap)
+  logoCommands.add_command(add_image)
+  
+  # Export the Icon
+  logoCommands.add_command(CommandModule.make_export(exporter))
+  Smig.perform_commands(logoCommands)
+end
+
+make_transparentpngicon()
+
+# drawTearDrop = make_drawlogo(370.0)
+# puts JSON.pretty_generate(drawTearDrop.elementhash)
