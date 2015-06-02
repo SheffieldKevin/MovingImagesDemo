043ef0e5288e0200d0b0faf473bc8511ffb9e6a5
Kevin Meaney <ktam@yvs.eu.com>
2015-06-02 23:19:35 +0100

    Removed dependence on variables and equations for rendering the MovingImages icon.

--- ZukiniIcon.rb
+++ ZukiniIcon.rb
@@ -3,29 +3,27 @@ require 'moving_images'
 include MovingImages
 include MICGDrawing
 
-def make_teardroppath()
+def make_teardroppath(iconwidth)
   thePath = MIPath.new
   centerPoint = MIShapes.make_point(0, 0)
   thePath.add_arc(centerPoint: centerPoint,
-         radius: "0.18352 * $iconwidth",
+         radius: 0.18352 * iconwidth,
      startAngle: 1.11631,
        endAngle: -1.11631)
 
-  centerPoint2 = MIShapes.make_point("$iconwidth * 0.38313", 0)
+  centerPoint2 = MIShapes.make_point(iconwidth * 0.38313, 0)
   thePath.add_arc(centerPoint: centerPoint2,
-         radius: "0.015325 * $iconwidth",
+         radius: 0.015325 * iconwidth,
      startAngle: -1.11631,
        endAngle: 1.11631)
   thePath.add_closesubpath()
 
-  startPoint = MIShapes.make_point(
-        "$iconwidth * 0.389858",
-        "$iconwidth * 0.0137695")
+  startPoint = MIShapes.make_point(iconwidth * 0.389858, iconwidth * 0.0137695)
   return startPoint, thePath
 end
 
-def make_drawteardrop(transformations)
-  startPoint, thePath = make_teardroppath()
+def make_drawteardrop(transformations, iconwidth)
+  startPoint, thePath = make_teardroppath(iconwidth)
   pathDrawElement = MIDrawElement.new(:fillpath)
   pathDrawElement.arrayofpathelements = thePath
   pathDrawElement.startpoint = startPoint
@@ -33,28 +31,29 @@ def make_drawteardrop(transformations)
   pathDrawElement
 end
 
-def make_drawlogo()
+def make_drawlogo(iconwidth)
   drawLogo = MIDrawElement.new(:arrayofelements)
   drawLogo.fillcolor = MIColor.make_rgbacolor(0.82, 0.53, 0.1)
 
-  offset = MIShapes.make_point("-$iconwidth * 0.480096", 0)
+  offset = MIShapes.make_point(-iconwidth * 0.480096, 0)
   6.times do |i|
     transformations = MITransformations.make_contexttransformation()
     angle = i.to_f * 60.0 / 180.0 * Math::PI
     MITransformations.add_rotatetransform(transformations, angle)
     MITransformations.add_translatetransform(transformations, offset)
-    drawElement = make_drawteardrop(transformations)
+    drawElement = make_drawteardrop(transformations, iconwidth)
     drawLogo.add_drawelement_toarrayofelements(drawElement)
   end
 
   transformationIcon = MITransformations.make_contexttransformation()
-  centrePoint = MIShapes.make_point("$width * 0.5",
-                                    "$height * 0.5")
+#  centrePoint = MIShapes.make_point("$width * 0.5", "$height * 0.5")
+  modIconWidth = iconwidth * 1.35 # 1/1.35 = 0.74074074
+  centrePoint = MIShapes.make_point(modIconWidth * 0.5, modIconWidth * 0.5)
   MITransformations.add_translatetransform(transformationIcon, centrePoint)
-  MITransformations.add_rotatetransform(transformationIcon, "$angle")
+  MITransformations.add_rotatetransform(transformationIcon, 0.526)
   drawLogo.contexttransformations = transformationIcon
   drawLogo
 end
 
-drawTearDrop = make_drawlogo()
+drawTearDrop = make_drawlogo(370.0)
 puts JSON.pretty_generate(drawTearDrop.elementhash)
